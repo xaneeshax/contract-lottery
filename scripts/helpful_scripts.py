daa5ce72b92f41fec19b3d1220cca7e447eed073
xaneeshax <64504643+xaneeshax@users.noreply.github.com>
2022-01-06 01:38:04 -0800

    first commit

---
+++ scripts/helpful_scripts.py
@@ -0,0 +1,82 @@
+from brownie import (
+    network, 
+    config, 
+    accounts, 
+    MockV3Aggregator,
+    VRFCoordinatorMock,
+    LinkToken,
+    Contract,
+    interface
+)
+from web3 import Web3
+
+FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
+
+
+
+def get_account(index=None, id=None):
+    if index:
+        return accounts[index]
+    if id:
+        return accounts.load(id)
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
+        or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
+    ):
+        return accounts[0]
+    return accounts.add(config["wallets"]["from_key"])
+
+
+contract_to_mock = {
+    "eth_usd_price_feed" : MockV3Aggregator,
+    "vrf_coordinator" : VRFCoordinatorMock,
+    "link_token" : LinkToken
+}
+
+
+def get_contract(contract_name):
+    
+    contract_type = contract_to_mock[contract_name]
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        if len(contract_type) <= 0:
+            # MockAggregator.length
+            deploy_mocks()
+        contract = contract_type[-1]
+    else:
+        contract_address = config["networks"][network.show_active()][contract_name]
+        contract = Contract.from_abi(
+            contract_type._name, contract_address, contract_type.abi
+        )
+    
+    return contract
+
+
+
+DECIMALS = 8
+INITIAL_VALUE = 2000 * 10 ** 8
+
+def deploy_mocks(decimals=DECIMALS, initial_value=INITIAL_VALUE):
+    account = get_account()
+    MockV3Aggregator.deploy(
+        decimals, initial_value, {"from": account}
+    )
+    link_token = LinkToken.deploy({"from": account})
+    VRFCoordinatorMock.deploy(link_token, {"from" :account})
+    print("Mocks Deployed!")
+
+
+def fund_with_link(contract_address, account=None, link_token=None, amount=10**17):
+    account = account if account else get_account()
+    link_token = link_token if link_token else get_contract("link_token")
+    #link_token_contract = interface.LinkTokenInterface(link_token.address)
+    #tx = link_token_contract.transfer(contract_address, amount, {"from" : account})
+    tx = link_token.transfer(contract_address, amount, {"from" : account})
+    tx.wait(1)
+    print("Fund contract!")
+    return tx
+
+
+
+
+
